0e679e06f46b5f5c5e2dd718d64d583c8a08c2a5
crooss <crooss@hotmail.com>
2025-03-26 12:02:03 -0600

    Rename "page3" to "Geometry" in navigation for clarity and consistency

--- app.py
+++ app.py
@@ -4,28 +4,27 @@ import streamlit as st
 
 # Main entry point for the Streamlit application
 def main():
-    st.title("Streamlit Web Application")
-    st.sidebar.title("Navigation")
+    st.title("Web Application")
+    # st.sidebar.title("Navigation")
     
     # Navigation options
     pages = {
         "Grafica Horaria": "pages/Grafica_horaria",
         "Conductancia específica": "pages/Conductancia_específica",
-        "page3": "pages/page3"
+        "Geometry": "pages/page3"
     }
     
-    selection = st.sidebar.radio("Go to", list(pages.keys()))
+    selection = st.sidebar.radio("Go to", list(pages.keys()), index=None)
     page = None
-    # print(list(pages.keys()))
+
     # Load the selected page
     if selection == "Grafica Horaria":
         import pages.Grafica_horaria as page
-        print(page)
         page.Grafica_horaria()
     elif selection == "Conductancia específica":
         import pages.Conductancia_específica as page
         page.Conductancia_específica()
-    elif selection == "page3":
+    elif selection == "Geometry":
         import pages.page3 as page
         page.page3()
     
