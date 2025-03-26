848fe91049e9ae92d2f5396db0997653cfb34e66
crooss <crooss@hotmail.com>
2025-03-26 10:42:43 -0600

    Remove commented-out code in page3.py for improved readability

--- pages/page3.py
+++ pages/page3.py
@@ -25,21 +25,7 @@ def page3():
         if option:
             dataframe = pd.read_excel(uploaded_file, sheet_name=option)
             st.write(dataframe)
-    #     # To read file as bytes:
-    #     bytes_data = uploaded_file.getvalue()
-    #     st.write(bytes_data)
 
-    #     # To convert to a string based IO:
-    #     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    #     st.write(stringio)
-
-    #     # To read file as string:
-    #     string_data = stringio.read()
-    #     st.write(string_data)
-
-    #     # Can be used wherever a "file-like" object is accepted:
-
-    #     st.write(dataframe)
 
 if __name__ == "__main__":
     page3()
