d3c095b91cd1418a89352ab3bc0e84eb1f4afd2b
crooss <crooss@hotmail.com>
2025-04-02 14:43:11 -0600

    Enhance user interface in Geometry_Dimension_Class.py with improved captions and placeholders for better data input guidance

--- pages/Geometry_Dimension_Class.py
+++ pages/Geometry_Dimension_Class.py
@@ -6,27 +6,28 @@ def Geometry_Dimension_Class():
     st.title("Anomaly Dimesion Class")
     st.subheader("Esta aplicacion ayuda a la creacion de un grafico de clasificaciones de dimensiones de corridas instrumentadas")
     st.write("Los datos necesarios para la creacion del grafico son:")
-    st.caption("Longitud de anomalia en mm")
-    st.caption("Ancho de anomalia en mm")
-    st.caption("Espesor nominal en mm")
-
+    st.markdown(''':orange[Longitud de anomalia en mm]  
+                   :orange[Ancho de anomalia en mm]  
+                   :orange[Espesor nominal en mm]''')
+    
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         # List all sheet names in the uploaded Excel file
         xl = pd.ExcelFile(uploaded_file)
         sheet_names = xl.sheet_names
         # st.write(sheet_names)
-        option = st.selectbox('Seleccionar la Hoja de origen de datos',(sheet_names), index=None)
+        option = st.selectbox('Seleccionar la Hoja de origen de datos',(sheet_names), index=None, placeholder="Seleccione la hoja de datos")
         if option:
             dataframe = pd.read_excel(uploaded_file, sheet_name=option)
             df=pd.DataFrame(dataframe)
-            st.write(dataframe)
+            with st.expander("Ver datos de entrada"):
+                st.write(dataframe)
             
-            option_columns_L = st.selectbox('Seleccionar la columna que contiene la longitud del defecto en mm', dataframe.columns, index=None)
+            option_columns_L = st.selectbox('Seleccionar la columna que contiene la longitud del defecto en mm', dataframe.columns, index=None, placeholder="Seleccione Longitud en mm")
             if option_columns_L:
-                option_columns_W=st.selectbox('Seleccionar la columna que contiene el ancho del defecto en mm', dataframe.columns, index=None)
+                option_columns_W=st.selectbox('Seleccionar la columna que contiene el ancho del defecto en mm', dataframe.columns, index=None, placeholder="Seleccione Ancho en mm")
                 if option_columns_W:
-                    option_columns_t=st.selectbox('Seleccionar la columna que contiene el espesor nominal en mm', dataframe.columns, index=None)
+                    option_columns_t=st.selectbox('Seleccionar la columna que contiene el espesor nominal en mm', dataframe.columns, index=None, placeholder="Seleccione Espesor en mm")
                     if option_columns_t and option_columns_L and option_columns_W:
                         extent_xy=st.radio("Extensión del eje x y eje y", [10,20,30], index=None)
                         if extent_xy:
