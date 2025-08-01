8a7cf5b4236ac6c80f8545a90e4500cbf422179e
crooss <crooss@hotmail.com>
2025-08-01 11:11:52 -0600

    hide

--- app.py
+++ app.py
@@ -46,3 +46,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
