8f2f812f13436365263f64fc3dfd73f69fc7b589
crooss <crooss@hotmail.com>
2026-05-21 11:33:12 -0600

    Agregar nueva página para graficar la posición horaria de una anomalía en la aplicación Streamlit

---
+++ pages/Conversion_xls_a_Shapefile.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import math
+from funciones import graficar, texto, radianes_a_horas
+matplotlib.use('agg')
+
+
+def xls_a_shp():
+    st.title("Realizar gráfico de posición horaria de una anomalía")
+    nombre=st.text_input("Nombre de la anomalía", value="#1")
+    angulo_inicio=st.number_input("Ángulo inicial [°]", min_value=0.0, max_value=360.0, format="%.4f", step=0.0001)
+    angulo_final=st.number_input("Ángulo final [°]",min_value=0.0, max_value=360.0, format="%.4f", step=0.0001)
+    PM=st.number_input("Pérdida de Metal [%]")/100
+    if angulo_final and nombre and PM:
+        graficar(angulo_inicio,angulo_final,nombre,PM)
+        # Display the saved figure in Streamlit
+        st.image('images/fig.png', caption='Generated Plot', use_container_width=True)
+        # st.pyplot(graficar(angulo_inicio,angulo_final,nombre,PM)[1])
+if __name__ == "__main__":
+    xls_a_shp()
