cde2914a12f62b0833b6517012a3ee7d169fc8ee
crooss <crooss@hotmail.com>
2026-05-21 14:56:10 -0600

    a

--- pages/Conversion_xls_a_Shapefile.py
+++ pages/Conversion_xls_a_Shapefile.py
@@ -85,7 +85,7 @@ def xls_a_shp():
     if creado==1:
         graf= st.checkbox("Mostrar shapefile en mapa")
         st.write(seleccion_X, seleccion_Y)
-        dataframe_gdf = gdf[gdf[seleccion_X, seleccion_Y]]
+        # dataframe_gdf = gdf[gdf[seleccion_X, seleccion_Y]]
         dataframe_gdf = gdf.rename(columns={seleccion_X: "longitude", seleccion_Y: "latitude"})
         
         if graf and creado==1:
